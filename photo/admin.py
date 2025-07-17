1db49f6ae0990509ab788bc7a235646fb4561605
httpablo <pablobrith@gmail.com>
2025-07-17 12:54:55 -0300

    TabularInline

--- photo/admin.py
+++ photo/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from . import models
+from comment.models import Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
 
 
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'name', 'img', 'accesses',)
     search_fields = ('user', 'name',)
+    inlines = (CommentInline,)
 
 
 admin.site.register(models.Photo, PhotoAdmin)
--- photo/urls.py
+++ photo/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('photos/', views.PhotoCreateListAPIView.as_view(), name='photo-create-list-api-view'),
-    path('photos/<int:pk>/', views.PhotoRetrieveUpdateDestroyAPIView.as_view(), name='photo-detail-api-view'),
+    path('photo/<int:pk>/', views.PhotoRetrieveUpdateDestroyAPIView.as_view(), name='photo-detail-api-view'),
 ]
